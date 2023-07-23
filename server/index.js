b8c1c1d1395afeea6553a721a75e1467017b096e
Sripathy <20tuee127@skct.edu.in>
2023-07-23 20:19:29 +0530

    First Commit
    Xrip chat app is a real time chat app created using MERN stack and Socket.io

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+const username = 'admin';
+const password = 'wM1ssBbGYT9xdHbk';
+const databaseName = 'chat';
+const collectionName = 'Users';
+const connectionString = `mongodb+srv://${username}:${password}@cluster0.tbbuekc.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+const PORT=5000;
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
